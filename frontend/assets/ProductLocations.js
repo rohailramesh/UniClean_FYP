fd9aa43538b71d3c4fe7fcfd5e48fff819747e04
Rohail Ramesh <rohailramesh@hotmail.com>
2023-12-26 15:05:20 +0000

    Feat: Added maps and location markers

---
+++ frontend/assets/ProductLocations.js
@@ -0,0 +1,58 @@
+export const LocationMarkers = [
+  {
+    latitude: 51.524208438335194,
+    longitude: -0.039884448051452644,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Mile End Library",
+  },
+  {
+    latitude: 51.52360765654108,
+    longitude: -0.04072129726409913,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Queens' Building - Toilet first floor",
+  },
+  {
+    latitude: 51.523657721993324,
+    longitude: -0.039176344871521,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Students' Union Hub - Toilets",
+  },
+  {
+    latitude: 51.524655681857375,
+    longitude: -0.04035651683807374,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Francis Bancroft Building - First Floor Toilets",
+  },
+  {
+    latitude: 51.52251621600215,
+    longitude: -0.043060183525085456,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Maths Building - Upper First Floor Toilets",
+  },
+  {
+    latitude: 51.52421511364394,
+    longitude: -0.03811955451965333,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Arts One Building - Ground Floor Toilets",
+  },
+  {
+    latitude: 51.523374017036076,
+    longitude: -0.041332840919494636,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Graduate Centre - Second Floor Toilets",
+  },
+  {
+    latitude: 51.52320713094144,
+    longitude: -0.04191219806671143,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    title: "Graduate Centre - Sevent Floor Toilets",
+  },
+];
--- frontend/screens/Products.js
+++ frontend/screens/Products.js
@@ -1,42 +1,68 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 import { supabase } from "../lib/supabase";
 import { Button } from "react-native-elements";
+import { LocationMarkers } from "../assets/ProductLocations";
 
 export default function Products({ session }) {
   // const user = session?.user;
 
   return (
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <Text style={styles.welcomeText}>Hygiene Products Screen</Text>
-        <Button
-          containerStyle={styles.buttonContainer}
-          buttonStyle={styles.button}
-          titleStyle={styles.buttonText}
-          onPress={() => supabase.auth.signOut()}
-          title="Sign Out"
-        />
-      </ScrollView>
+      <MapView
+        style={styles.map}
+        // initial region is the current location of the user
+        initialRegion={{
+          latitude: 51.523220481851524,
+          longitude: -0.04035115242004395,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider="google"
+      >
+        {LocationMarkers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+            title={marker.title}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     // flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     marginTop: 50,
+//   },
+//   welcomeText: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     color: "black",
+//     marginBottom: 20,
+//   },
+//   buttonContainer: {
+//     width: "80%",
+//     marginVertical: 20,
+//   },
+// });
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
-    marginTop: 50,
+    justifyContent: "center",
   },
-  welcomeText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "black",
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    width: "80%",
-    marginVertical: 20,
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
